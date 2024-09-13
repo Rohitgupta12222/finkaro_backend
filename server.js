344c1292f222ae93a9cf9bfafaf97ac972fb5328
Rohitgupta1222 <rohitg@techsimians.com>
2024-09-13 21:23:02 +0530

    rollback

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
+const Razorpay = require('razorpay');
 const cors = require('cors');
 const path = require('path');
-require('dotenv').config();
 require('dotenv').config();
 // Start server on specified port (from .env or default 3000)
 const PORT = process.env.PORT || 3000;
 const db = require('./db');
 
 app.use(express.json());
+
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
 
 console.log('Allowed Origins:', process.env.FRONTEND_LINK, process.env.FRONTEND_LINK_LOCAL);
@@ -31,6 +33,25 @@ app.use((req, res, next) => {
   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
   next();
 });
+
+// Routers
+const userRouter = require('./routers/userRouter');
+const subscribeRouter = require('./routers/subscribeRouter');
+const blogRouter = require('./routers/blogRouter');
+const youtubeRouter = require('./routers/youtubelinkRouter');
+const courseRouter = require('./routers/courseRouter');
+// const razorpayRouter = require('./routers/razorpayRouter');
+const dashboardRouter = require('./routers/dashboardRouter')
+
+
+// Define routes
+app.use('/user', userRouter);
+app.use('/subscribe', subscribeRouter);
+app.use('/blog', blogRouter);
+app.use('/youtubelink', youtubeRouter);
+app.use('/course', courseRouter);
+// app.use('/payment', razorpayRouter);
+app.use('/dashboard', dashboardRouter);
 
 
 
