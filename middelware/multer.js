dc51c0fcdf75d26e09af3aa5eafff8e924ecc4d1
Rohitgupta1222 <rohitg@techsimians.com>
2024-09-13 12:37:13 +0530

    webp

--- middelware/multer.js
+++ middelware/multer.js
@@ -1,12 +1,30 @@
-// uploadMiddleware.js
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+const webp = require('webp-converter');
 
 // Set storage engine
 const storage = multer.diskStorage({
   destination: './public/uploads/',
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    const fileName = file.fieldname + '-' + Date.now() + path.extname(file.originalname);
+    cb(null, fileName);
+
+    // Convert the uploaded image to WebP format
+    const inputPath = path.join('./public/uploads/', fileName);
+    const outputPath = path.join('./public/uploads/', file.fieldname + '-' + Date.now() + '.webp');
+
+    // Convert image to WebP
+    webp.cwebp(inputPath, outputPath, '-q 80', (status, error) => {
+      if (status === 'error') {
+        console.log('Error converting image to WebP:', error);
+      } else {
+        // Remove the original image after conversion
+        fs.unlink(inputPath, (err) => {
+          if (err) console.error('Error deleting original image:', err);
+        });
+      }
+    });
   }
 });
 
@@ -26,7 +44,7 @@ function checkFileType(file, cb) {
 // Initialize upload middleware
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 1000000000000 }, // 1MB file size limit
+  limits: { fileSize: 1 * 1024 * 1024 }, // 1MB file size limit
   fileFilter: function (req, file, cb) {
     checkFileType(file, cb);
   }
