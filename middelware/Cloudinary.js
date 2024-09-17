022935ebf3da9b655ccd45aecfdd3e1ac234dd1a
Rohitgupta1222 <rohitg@techsimians.com>
2024-09-17 13:04:34 +0530

    ddd

--- middelware/Cloudinary.js
+++ middelware/Cloudinary.js
@@ -1,11 +1,11 @@
 // cloudinary.js
 const cloudinary = require('cloudinary').v2;
-
+require('dotenv').config();
 // Configure Cloudinary with your credentials
 cloudinary.config({
-  cloud_name: 'dabv3eznr',
-  api_key: '588914789783632',
-  api_secret: 'S6Cn4VUgYDt8J2WxxkpjHeXvmUI'
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 module.exports = cloudinary;
