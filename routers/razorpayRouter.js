ef1adeb55a145a48b83d9b6c69187936f93387d2
Rohitgupta1222 <rohitg@techsimians.com>
2024-09-18 15:54:04 +0530

    remove config

--- routers/razorpayRouter.js
+++ routers/razorpayRouter.js
@@ -5,8 +5,6 @@ require('dotenv').config();
 const sendRegistrationEmail = require('../mail/registerMail'); // Adjust path to your mailer fil
 const Razorpay = require('razorpay');
 const Order = require('../models/order'); // Import the Order model
-const { default: payments } = require('razorpay/dist/types/payments');
-const BASE_URL = process.env.BASE_URL; // Change this to your actual base URL
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_id,
   key_secret: process.env.RAZORPAY_SECRET_KEY,
@@ -15,11 +13,7 @@ const razorpay = new Razorpay({
 
 router.post('/create-order', async (req, res) => {
   console.log('calling data razorpay');
-  const razorpay = new Razorpay({
-    key_id:process.env.RAZORPAY_id,
-    key_secret:process.env.RAZORPAY_SECRET_KEY
-  })
-  
+
   const option ={
     amount:req.body.amount,
     currency:req.body.currency,
@@ -43,10 +37,7 @@ router.post('/create-order', async (req, res) => {
 });
 router.get('/payment/:paymentId', async (req,res)=>{
   const paymentId = req.params;
-  const razorpay = new Razorpay({
-    key_id:process.env.RAZORPAY_id,
-    key_secret:process.env.RAZORPAY_SECRET_KEY
-  })
+
   try {
     const payment =await razorpay.payments.fetch(paymentId);
     if(!payment)return res.status(500).json('Error at Razorpay loading');
