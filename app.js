80fbdc3d83ee8c3392ee9e6687280ed3b30d2070
Rohitgupta <rohitgupta12031993@gmail.com>
2024-11-10 21:10:23 +0530

    cors error

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const db = require('./db');
 
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
