84893bfdb242eb50d4b22b3a33f05eac62b6aff9
Rohitgupta <rohitgupta12031993@gmail.com>
2024-11-10 20:54:09 +0530

    cors error

--- app.js
+++ app.js
@@ -12,10 +12,9 @@ app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
 
-console.log('Allowed Origins:',);
 
 app.use(cors({
-  origin: '*', 
+  origin: 'https://finkaro.com', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
