7d848bcfd68f146c2bfdb31be252158a5a4b723e
Rohitgupta <rohitgupta12031993@gmail.com>
2024-11-11 16:26:22 +0530

    add cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
 
 app.use(cors({
-  origin: ['https://finkaro.com','http://localhost:4200'], 
+  origin: ['*'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
