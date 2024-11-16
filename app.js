2b9cb6c12823d5af34ad8bc169052b5d2f1363c4
Rohitgupta <rohitgupta12031993@gmail.com>
2024-11-17 00:31:38 +0530

    changes mb

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const db = require('./db');
 
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ limit: '100mb', extended: true }));
-
+app.use(express.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
 
