d5fda58bbee0c27b10f5e5280afb562c57f9ab70
Rohit gupta <rohitgupta12031993@gmail.com>
2025-02-15 18:47:08 +0530

    fixed db connection

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
-// const mongoDbURL = process.env.DB_URL_LOCAL;
- const mongoDbURL = process.env.DB_URL;
-mongoose.connect(`${mongoDbURL}`,{
+const mongoDbURL = process.env.DB_URL;
+mongoose.connect(`${mongoDbURL}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverSelectionTimeoutMS: 30000,
-        socketTimeoutMS: 45000, 
+    socketTimeoutMS: 45000,
 })
 const db = mongoose.connection;
-db.on('connected',()=> console.log('mongo database connected'));
-db.on('error', async(err)=>{ console.log('mongo database error ',err);
+db.on('connected', () => console.log('mongo database connected'));
+db.on('error', async (err) => {
+    console.log('mongo database error ', err);
     await mongoose.connection.close();
     console.log('Connection closed due to error.');
     process.exit(1); // Exit with failure
 });
-db.on('disconnected',()=> console.log('mongo database disconnected'))
+db.on('disconnected', () => console.log('mongo database disconnected'))
 module.exports = db
